bda3cfedfc5c7268def33bc2fe9c58dfe620883d
Rizoren <rizoren@yandex.ru>
2019-08-29 11:42:58 +0500

    ins

---
+++ Lesson_01/src/main/java/ru/otus/spring01/Main.java
@@ -0,0 +1,15 @@
+package ru.otus.spring01;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.spring01.domain.Person;
+import ru.otus.spring01.service.PersonService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        PersonService s = context.getBean(PersonService.class);
+        Person ivan = s.getByName("Ivan");
+        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+    }
+}
