e7355a78d32ecc2a743d0ed7cf26bb8027c2c54b
Rizoren <rizoren@yandex.ru>
2019-09-13 23:21:41 +0500

    Homework 03 pre

---
+++ Homework_03/src/test/java/ru/otus/springhomework03/SpringHomework03ApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.otus.springhomework03;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHomework03ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
