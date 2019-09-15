1ca8ac6c30d4a682fc8bb774855dbdf900bf0c1b
Rizoren <rizoren@yandex.ru>
2019-09-15 13:28:22 +0500

    Homework 03 done

--- Homework_03/src/main/java/ru/otus/springhomework03/SpringHomework03Application.java
+++ Homework_03/src/main/java/ru/otus/springhomework03/SpringHomework03Application.java
@@ -3,15 +3,14 @@ package ru.otus.springhomework03;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import ru.otus.springhomework03.services.ConsolePolling;
 import ru.otus.springhomework03.services.ConsolePollingImpl;
 
 @SpringBootApplication
 public class SpringHomework03Application {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext context = SpringApplication.run(SpringHomework03Application.class, args);
-		ConsolePollingImpl exam = context.getBean("exam", ConsolePollingImpl.class);
-		exam.run();
+		SpringApplication.run(SpringHomework03Application.class, args).getBean("exam",ConsolePolling.class).run();
 	}
 
 }
