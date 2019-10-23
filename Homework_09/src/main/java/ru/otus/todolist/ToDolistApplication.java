c61a103f08b85ad1ec0d577e353f4246502d5c6c
Rizoren <rizoren@yandex.ru>
2019-10-23 21:48:34 +0500

    Homework 09 pre-ToDo List

---
+++ Homework_09/src/main/java/ru/otus/todolist/ToDolistApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDolistApplication.class, args);
+    }
+
+}
