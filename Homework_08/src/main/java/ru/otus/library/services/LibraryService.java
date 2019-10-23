326a380e6e9355b2308372cab02807b68a0976b6
Rizoren <rizoren@yandex.ru>
2019-10-24 00:40:03 +0500

    Revert "Merge branch 'master' of https://github.com/Rizoren/2019-08-otus-spring-Butygin"
    This reverts commit 5eeea057c5fae4cc1c445804ddce7de906c335ce, reversing
    changes made to 8c5cde91f0e9d854c4a14bc7a8b7cfa20b6f3b87.

--- Homework_08/src/main/java/ru/otus/library/services/LibraryService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.library.services;
-
-public interface LibraryService {
-
-    void showInfo();
-    void showAllBooksByGenre(String genre);
-    void showAllBooksByAuthorID(String id);
-    void insertAuthor();
-}
--- Homework_08/src/main/java/ru/otus/library/util/RawResultPrinter.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.library.util;
-
-import org.bson.Document;
-
-public interface RawResultPrinter {
-    @SuppressWarnings("unchecked")
-    void prettyPrintRawResult(Document document);
-}
